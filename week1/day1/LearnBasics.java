3eb4ed1798930da5df265a315ba28a02e58dfde7
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-04-17 13:11:19 +0530

    Week 1 Code Updated

--- week1/day1/LearnBasics.java
+++ week1/day1/LearnBasics.java
@@ -3,10 +3,10 @@ package week1.day1;
 public class LearnBasics {
 // Primitive Data types - Mobile
 // Datatype vairableName = value;
-	byte pixelSize = 100;
-	short internalStorage = 32000 ;
+	public byte pixelSize = 100;
+	private short internalStorage = 32000 ;
 	int imei = 987654321;
-	long mobileNumber = 9987655432L;
+	protected long mobileNumber = 9987655432L;
 	
 	float secondaryPixelSize = 4345.8345111F;
 	double kernalNumber = 155647.994838748757;
