427c35954232c362b03892f8a94a9662c2cf75b3
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-05-22 13:30:02 +0530

    Week 5 code

--- week5/day1/LearnAttributes.java
+++ week5/day1/LearnAttributes.java
@@ -1,6 +1,5 @@
 package week5.day1;
 
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class LearnAttributes {
@@ -9,18 +8,21 @@ public class LearnAttributes {
 		System.out.println("Test 1");
 	}
 	
-	@Test
+	@Test(dependsOnMethods = {"week5.day1.LearnAttributes.c"}, alwaysRun = true)
 	public void a() {
 		System.out.println("Test 2");
 	}
 	
-	@Test(invocationCount = 7,invocationTimeOut = 5000,threadPoolSize = 2 )
+	@Test
 	public void c() throws InterruptedException {
 		Thread.sleep(2000);
 		System.out.println("Test 3");
+		throw new RuntimeException();
 	}
 	/*
 	 * (enabled = false) -> @Ignore
 	 * (priority = -1)
+	 * invocationCount = 7,invocationTimeOut = 5000,threadPoolSize = 2 
+	 * timeOut = 1000
 	 */
 }
--- week5/day1/ProjectSpecificMethods.java
+++ week5/day1/ProjectSpecificMethods.java
@@ -3,19 +3,30 @@ package week5.day1;
 import java.time.Duration;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ProjectSpecificMethods {
-	public ChromeDriver driver;
+	public RemoteWebDriver driver;
+	
+	@Parameters({"browserName","URL"})
 	@BeforeMethod
-	public void startBrowser() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+	public void startBrowser(String browserName, String url) {
+		if (browserName.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if(browserName.equals("Edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
+		
 		driver.manage().window().maximize();
-		driver.get("http://leaftaps.com/opentaps/");
+		driver.get(url);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 
